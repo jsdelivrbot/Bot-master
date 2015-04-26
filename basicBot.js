c3e960e1ea0cbcf57a28d6b23a31b4793646f9a3
Alexander <Alexanderpeter@live.dk>
2015-04-26 21:05:39 +0200

    d

--- basicBot.js
+++ basicBot.js
@@ -280,7 +280,7 @@
             autodisableInterval: null,
             autodisableFunc: function () {
                 if (basicBot.status && basicBot.settings.autodisable) {
-                    API.sendChat('!afkdisable');
+                    API.sendChat('afkdisable');
                     API.sendChat('!joindisable');
                 }
             },
