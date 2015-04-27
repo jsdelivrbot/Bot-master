a0437c09c69720adf41cc5e17d4bad26538ab77c
Alexander <Alexanderpeter@live.dk>
2015-04-27 11:40:23 +0200

    banners

--- basicBot.js
+++ basicBot.js
@@ -2626,6 +2626,19 @@
                 }
             },
 
+            bannerCommand: {
+                command: 'banners',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.banners)
+                    }
+                }
+            },
+
             purchaseCommand: {
                 command: ['purchase'],
                 rank: 'user',
