57daefbccf5988af5581ebed9e5d1fc15e2115c8
Alexander <Alexanderpeter@live.dk>
2015-04-27 12:20:45 +0200

    sa

--- basicBot.js
+++ basicBot.js
@@ -2647,7 +2647,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(basicBot.chat.rekt)
+                        API.sendChat(basicBot.chat.rekted)
                     }
                 }
             },
