220868ae6f318ec835a68a452c631fb446150718
Alexander <Alexanderpeter@live.dk>
2015-04-28 16:25:03 +0200

    zerulez

--- basicBot.js
+++ basicBot.js
@@ -252,7 +252,7 @@
             etaRestriction: false,
             welcome: false,
             opLink: null,
-            rulesLink: null,
+            rulesLink: "http://pastebin.com/6NEPtZW9",
             themeLink: null,
             fbLink: null,
             youtubeLink: null,
