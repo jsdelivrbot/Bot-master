8886238481b7788e0d326f20c59384d1045c0677
Alexander <Alexanderpeter@live.dk>
2015-04-28 11:03:37 +0200

    update

--- basicBot.js
+++ basicBot.js
@@ -2717,6 +2717,19 @@
                 }
             },
 
+            streamCommand: {
+                command: 'stream',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.stream)
+                    }
+                }
+            },
+
 
             purchaseCommand: {
                 command: ['purchase'],
