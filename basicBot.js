96ef3b38eaf9412074d38bf504ec93617ff0bc69
Alexander <Alexanderpeter@live.dk>
2015-04-28 11:09:32 +0200

    s

--- basicBot.js
+++ basicBot.js
@@ -2730,6 +2730,19 @@
                 }
             },
 
+            gif2Command: {
+                command: 'gif',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.gif)
+                    }
+                }
+            },
+
 
             purchaseCommand: {
                 command: ['purchase'],
