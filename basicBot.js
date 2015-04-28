d712d85281d04038b22549744740535ea5e6b956
Alexander <Alexanderpeter@live.dk>
2015-04-28 11:52:25 +0200

    danmark

--- basicBot.js
+++ basicBot.js
@@ -2730,6 +2730,19 @@
                 }
             },
 
+            danmarkCommand: {
+                command: 'danmark',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.danmark)
+                    }
+                }
+            },
+            
 
 
             purchaseCommand: {
