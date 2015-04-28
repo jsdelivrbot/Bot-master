3b6f54b1f5c83c7cad3f52cd3a556a5d822dd38b
Alexander <Alexanderpeter@live.dk>
2015-04-28 14:44:23 +0200

    rules

--- basicBot.js
+++ basicBot.js
@@ -2742,6 +2742,19 @@
                     }
                 }
             },
+
+            rulesCommand: {
+                command: 'rules',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.rules)
+                    }
+                }
+            },
             
 
 
