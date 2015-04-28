432f45a44470efa90c86399597f5e8362ee33d8e
Alexander <Alexanderpeter@live.dk>
2015-04-28 10:58:15 +0200

    s

--- basicBot.js
+++ basicBot.js
@@ -2678,6 +2678,32 @@
                 }
             },
 
+            sanicCommand: {
+                command: 'sanic',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.sanic)
+                    }
+                }
+            },
+
+            cutesanicCommand: {
+                command: 'cutesanic',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.cutesanic)
+                    }
+                }
+            },
+
             kolentoCommand: {
                 command: 'kolento',
                 rank: 'user',
